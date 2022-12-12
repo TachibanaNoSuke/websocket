1809e0b8bbe326fedd4e8d5e393bf954299c34e3
TachibanaNoSuke <tachibananosuke@gmail.com>
2022-12-12 22:30:17 +0800

    [功能] 新增 log

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({
+    port: 8080
+});
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
+    console.log('server is start, port 8080');
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
 
-  ws.send('something');
+    ws.send('something');
 });
